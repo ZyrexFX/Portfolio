c7a2a458189a85e1eda63d54b9cd08d8de08e207
Kostilyev <felixferlians77@gmail.com>
2023-07-28 14:40:34 +0700

    a

--- js/cursor.js
+++ js/cursor.js
@@ -4,3 +4,6 @@ var cursor2 = document.querySelector('.cursor2')
 document.addEventListener('mousemove', function(e) {
     cursor.style.cssText = cursor2.style.cssText = 'left: ' + e.clientX + 'px; top: ' + e.clientY + 'px;';
 })
+
+// document.getElementById('body').style.cursor = 'none';
+// document.getElementById('home').style.cursor = 'none';
